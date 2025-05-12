3ab202da5f54ecf394a68ce518c7bc36d700a594
Angeles <ezequiel.cardozo2005@gmail.com>
2025-05-12 19:36:02 -0300

    [+] mejora de estilos [-] codigo redundante [!] reestructuracion en classes

--- resources/js/classes/rendererProducts.js
+++ resources/js/classes/rendererProducts.js
@@ -18,9 +18,12 @@ export class renderProd {
         categorias.forEach(cat => 
             this.renderCat(cat)
         );
+
+        // llama a la configuracion de eventos en el boton compra de cada producto
         this.setupBuyButtons();
     }
 
+    // Devuelve mensaje de error
     PrdNotFound() {
         this.container.innerHTML = `
         <div class="no-results">
@@ -28,6 +31,7 @@ export class renderProd {
         </div>`;
     }
 
+    // Render categoria
     renderCat(cat) {
         // Crea un contenedor para la categoria
         const categoriaSection = document.createElement('section');
@@ -53,6 +57,7 @@ export class renderProd {
         this.container.appendChild(categoriaSection);
     }
 
+    // Better Currency to local
     betterCTLstrg(value, moneda, producto) {
         const srchRegion = producto.getRegion(moneda);
         const srchCurrency = producto.getCurrency(srchRegion);
@@ -68,6 +73,7 @@ export class renderProd {
         }
     }
 
+    // Genera los datos de cada producto
     generateCardProd_x_Element(producto) {
         // Obtiene los precios en USD y ARS del producto
         // Nueva implementacion de busqueda y accesso
@@ -84,24 +90,17 @@ export class renderProd {
         return `
         <div class="producto-card" data-id="${producto.getId()}">
             <div class="producto-imagen-container">
+                ${producto.getStock() > 0 && producto.getStock() <= 3 ? '<span class="stock-badge">¡ultimas unidades!</span>' : ''}
                 <img src="..${producto.getImgSrc()}" alt="${producto.getDescripcion()}" class="producto-imagen"
                 onerror="this.src='../../resources/imgs/bsotd.jpg'">
-                ${producto.getStock() > 0 && producto.getStock() <= 3 ? '<span class="stock-badge">¡ultimas unidades!</span>' : ''}
             </div>
             <div class="producto-info">
                 <p class="producto-marca">${producto.getMarca()}</p>
                 <p class="producto-nombre">${producto.getNombre()}</p>
                 <p class="producto-descripcion">${producto.getDescripcion()}</p>
                 <div class="producto-meta">
-                    <div class="colores">
-                        <p class="colores-disponibles">Colores Disponibles</p>
-                        ${producto.getColores().map(color => `
-                          <span style="background-color: ${color.codigo};" title="${color.color}"></span>
-                        `).join('')}
-                    </div>
-                    <div class="metodos-pago">
-                        ${producto.getPaidMethod().map(m => `<span class="metodo-pago ${m}">${m}</span>`).join('')}
-                    </div>
+                    ${this.getGenColorHTML(producto)}
+                    ${this.getGenPaidMethod(producto)}
                 </div>
                 <div class="producto-precios">
                     ${detallesPPM.map(({ moneda, precio, descuento, PcD }) => `
@@ -113,16 +112,41 @@ export class renderProd {
                     `).join("")}
                 </div>
                 <div class="producto-acciones">
-                    <span class="stock ${producto.getStock() > 0 ? 'en-stock' : 'agotado'}">
-                      ${producto.getStock() > 0 ? '🟢 En stock' : '🔴 Agotado'}
-                    </span>
-                    <button class="btn-comprar" data-id="${producto.getId()}" data-idmodelo="${producto.getIdModelo()}">
-                      COMPRAR
-                    </button>
+                    ${this.getGenStockMethod(producto)}
                 </div>
             </div>
         </div>
     `;
+    }
+
+    getGenColorHTML(p) {
+        return `
+        <div class="colores">
+            <p class="colores-disponibles">Colores Disponibles</p>
+            ${p.getColores().map(color => `
+              <span style="background-color: ${color.codigo};" title="${color.color}"></span>
+            `).join('')}
+        </div>
+        `;
+    }
+
+    getGenPaidMethod(p) {
+        return `
+        <div class="metodos-pago">
+            ${p.getPaidMethod().map(m => `<span class="metodo-pago ${m}">${m}</span>`).join(',')}
+        </div>
+        `;
+    }
+
+    getGenStockMethod(p) {
+        return `
+        <span class="stock ${p.getStock() > 0 ? 'en-stock' : 'agotado'}">
+            ${p.getStock() > 0 ? '🟢 En stock' : '🔴 Agotado'}
+        </span>
+        <button class="btn-comprar" data-id="${p.getId()}" data-idmodelo="${p.getIdModelo()}">
+        COMPRAR
+        </button>
+        `;
     }
 
     setupBuyButtons() {
